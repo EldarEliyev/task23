d937f8c13120175f61feba0ecc64c2543955cfad
eldar23451 <liyev8024@gmail.com>
2024-05-30 23:05:10 +0400

    task file is added

---
+++ Customer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp53
+{
+    public class Customer
+    {
+        private string _name;
+        private int _fincode;
+        private string _surname;
+        public string surname
+        {
+            get
+            {
+                return _surname;
+            }
+
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("value");
+                } _surname = value;
+            }
+        }
+
+        public string name
+        {
+            get
+            {  
+                 return _name; 
+            }
+            set 
+            {  
+                if(string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("no name");
+                    name  = value;
+            }
+            }
+
+        }
+
+
+
+        public int fincode
+        {
+            get
+            {
+                return _fincode;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentNullException("fincode cannot be less than 0");
+                }
+                _fincode = value;
+                
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConsoleApp53;
+#region customer
+Customer customer = new Customer();
+customer.fincode = 1;
+customer.name = "eldar ";
+customer.surname = "eliyev";
+Console.WriteLine($"fincode:{customer.fincode}\nname:{customer.name}\nsurname:{customer.surname}");
+#endregion
+
+
+
